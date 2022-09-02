87a395096194998e3842df2fdeb9f1349a264c62
Nathan Samson <nathan@nathansamson.be>
2022-09-02 20:51:31 +0200

    Print C02 values every 2 seconds

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
+use embedded_hal_0_2::blocking::delay::DelayMs;
+
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::serial;
+use esp_idf_hal::prelude::*;
+
+use nb::block;
+
+use mh_z19c::MhZ19C;
+
 fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
     // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
+    let peripherals = Peripherals::take().unwrap();
+    let pins = peripherals.pins;
+
+    let config = serial::config::Config::default().baudrate(Hertz(9600));
+
+    let serial: serial::Serial<serial::UART2, _, _> = serial::Serial::new(
+        peripherals.uart2,
+        serial::Pins {
+            tx: pins.gpio26,
+            rx: pins.gpio15,
+            cts: None,
+            rts: None,
+        },
+        config
+    ).unwrap();
+
+    let mut co2sensor = MhZ19C::new(serial);
+
+    loop {
+        let co2 = block!(co2sensor.read_co2_ppm()).unwrap();
+        println!("CO2 value = {}ppm", co2);
+        FreeRtos.delay_ms(2000 as u32);
+    }
 }
